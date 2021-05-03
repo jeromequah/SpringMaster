12d4bd309e6735ccb65d7d5095b178de9c7d6c17
jeromequah <jerome.quah.2018@sis.smu.edu.sg>
2021-05-03 17:13:55 +0800

    Initial commit

---
+++ src/main/java/com/example/SpringMaster/SpringMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringMaster/SpringMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
