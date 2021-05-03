528cefa36031a8d0240bd906c3311ce558b60b58
jeromequah <jerome.quah.2018@sis.smu.edu.sg>
2021-05-03 21:36:50 +0800

    Edited @Component to specific annotation @Repository, @Service

--- src/main/java/com/example/SpringMaster/Admin/AdminRepository.java
+++ src/main/java/com/example/SpringMaster/Admin/AdminRepository.java
@@ -1,18 +1,31 @@
 package com.example.SpringMaster.Admin;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Collections;
 import java.util.List;
 
 // Admin - DATA ACCESS Layer
 
 // AdminRepository is used to connect to database
-@Component
+@Repository // Spring creates new instance of AdminRepository
+@Primary // When @Autowired at SERVICE Layer, this Primary implementation will be used
 public class AdminRepository implements AdminRepo {
     @Override
     public List<Admin> getAdmins() {
         // TODO connect to real db
-        return Collections.emptyList();
+        return Collections.singletonList(
+                new Admin(
+                        1,
+                        "TODO. Implement real database",
+                        "Implementrealdata@msn.com",
+                        LocalDate.of(1997, Month.MARCH,28),
+                        "98765432",
+                        "password")
+        );
     }
 }
