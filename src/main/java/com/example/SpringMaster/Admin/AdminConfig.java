54c01502ca924923aba0622eb93b557ae5532b51
jeromequah <jerome.quah.2018@sis.smu.edu.sg>
2021-05-06 02:53:07 +0800

    @Value @Environment Config, Spring Profiles for local & dev --spring.profiles.active=dev

--- src/main/java/com/example/SpringMaster/Admin/AdminConfig.java
+++ src/main/java/com/example/SpringMaster/Admin/AdminConfig.java
@@ -1,9 +1,12 @@
 package com.example.SpringMaster.Admin;
 
+import com.example.SpringMaster.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AdminConfig {
@@ -11,11 +14,20 @@ public class AdminConfig {
     @Value("${app.useFakeAdminRepo:false}") // default will be false for this variable
     private Boolean useFakeAdminRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
     @Bean
         // Initialises Spring, any code in args will be executed
-    CommandLineRunner commandLineRunner() { // used upon App start
+    CommandLineRunner commandLineRunner(InfoApp infoApp) { // used upon App start
         return args -> {
-            System.out.println("Command line runner yay");
+            System.out.println("Command line runner");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
+            System.out.println(infoApp);
         };
     }
 
