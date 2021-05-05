f54239662fea5852017d8c5d6e67844e002ff41a
jeromequah <jerome.quah.2018@sis.smu.edu.sg>
2021-05-06 02:07:16 +0800

    LOGGER Interface

--- src/main/java/com/example/SpringMaster/Admin/AdminService.java
+++ src/main/java/com/example/SpringMaster/Admin/AdminService.java
@@ -1,6 +1,9 @@
 package com.example.SpringMaster.Admin;
 
 import com.example.SpringMaster.exception.NotFoundException;
+import org.hibernate.annotations.common.util.impl.Log_$logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +14,10 @@ import java.util.List;
 @Component // creates a Bean to inject in multiple places with the SAME instance
 public class AdminService {
 
+    //    Logging for AdminService Class
+    private final static Logger LOGGER = // must be the Interface slf4j
+            LoggerFactory.getLogger(AdminService.class);
+
     // Reference to Admin - DATA ACCESS Layer via Interface
     private final AdminRepository adminRepository;
 
@@ -18,11 +25,12 @@ public class AdminService {
     @Autowired
     public AdminService(
 //            @Qualifier("fake") Annotation for using FakeRepository as its value = "fake"
-                    AdminRepository adminRepository) {
+            AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
     }
 
     List<Admin> getAdmins() {
+        LOGGER.info("getAdmins called");
         return adminRepository.findAll();
     }
 
@@ -30,6 +38,11 @@ public class AdminService {
         return adminRepository
                 .findById(id)
                 .orElseThrow(
-                        () -> new NotFoundException("Admin with id " + id + " Not Found"));
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "Admin with id " + id + " Not Found");
+                            LOGGER.error("Error for admin {}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 }
--- src/main/java/com/example/SpringMaster/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/SpringMaster/exception/ApiExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.SpringMaster.exception;
 
+import com.example.SpringMaster.Admin.AdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +13,10 @@ import java.time.ZonedDateTime;
 // Handles > 1 Exception
 @ControllerAdvice // To allow class to be used throughout whole application for exceptions
 public class ApiExceptionHandler {
+
+    //    Logging for ApiExceptionHandler Class
+    private final static Logger LOGGER =
+            LoggerFactory.getLogger(AdminService.class);
 
     @ExceptionHandler(value = ApiRequestException.class) // to handle more than 1 exception
     public ResponseEntity<Object> handleApiRequestException(
