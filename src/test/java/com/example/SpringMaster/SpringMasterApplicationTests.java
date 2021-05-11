d92d75343b6944a4b5eeaac79e6b970d9fecccfd
jeromequah <jerome.quah.2018@sis.smu.edu.sg>
2021-05-11 21:44:27 +0800

    Sample Unit Test for AdminService (To Debug)

--- src/test/java/com/example/SpringMaster/SpringMasterApplicationTests.java
+++ src/test/java/com/example/SpringMaster/SpringMasterApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.SpringMaster;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Uses JUnit
 @SpringBootTest
 class SpringMasterApplicationTests {
 
+//	Integration Test - All functionalities are functional
 	@Test
 	void contextLoads() {
 	}
